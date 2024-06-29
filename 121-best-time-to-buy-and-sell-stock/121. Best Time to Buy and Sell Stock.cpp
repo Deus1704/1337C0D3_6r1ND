06341bc4d7369e6e5065d1dbb25ad4e507b67014
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2024-06-29 19:37:09 +0530

    Runtime: 76 ms (88.98%), Memory: 95.8 MB (32.88%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int max_profit = 0;
+        int buy = prices[0];
         int n = prices.size();
-        int l=0,r=0;
-        while (r<n){
-            if (prices[r]>prices[l]) max_profit = max(max_profit, prices[r]-prices[l]);
-            else{
-                l=r;
-            }
-            r++;
+        for (int i=1; i<n; i++){
+            if (prices[i]<buy) buy = prices[i];
+            else if (prices[i]-buy > max_profit) max_profit = prices[i]-buy; 
         }
         return max_profit;
     }
