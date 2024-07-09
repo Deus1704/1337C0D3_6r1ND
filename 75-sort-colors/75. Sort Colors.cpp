0ffe594c864ea3d2c690b34fbfe6b30bf82395f4
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2024-07-10 01:21:25 +0530

    Runtime: 0 ms (100.00%), Memory: 9.9 MB (36.38%) - Gitcode

--- 75-sort-colors/75. Sort Colors.cpp
+++ 75-sort-colors/75. Sort Colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end()); //lol?
+        // sort(nums.begin(),nums.end()); //lol?
+        // DNF Algo
+        int leftptr =0;
+        int highptr = nums.size()-1;
+        int midptr =0;
+        while (highptr >= midptr){
+            switch (nums[midptr]){
+                case 0:
+                    swap(nums[leftptr++], nums[midptr++]);
+                    break;
+                case 1:
+                    midptr++;
+                    break;
+                case 2:
+                    swap(nums[midptr], nums[highptr--]);
+                    break;
+            }
+        }
     }
 };
