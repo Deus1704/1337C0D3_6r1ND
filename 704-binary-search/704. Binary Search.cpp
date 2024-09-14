9f87cd656ab7e57ddbeb349781d8ad58ec3e10db
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2024-09-14 16:15:15 +0530

    Runtime: 27 ms (57.16%), Memory: 30.3 MB (24.15%) - Gitcode

--- 704-binary-search/704. Binary Search.cpp
+++ 704-binary-search/704. Binary Search.cpp
@@ -1,19 +1,35 @@
-int binarysearch(vector<int>&nums, int target, int low, int high){
-    if (low>high){
-        return -1;
-    }
-    int mid = (low+high)/2;
-    if (nums[mid]==target) return mid;
-    else if (nums[mid]>target){
-        return binarysearch(nums,target,low,mid-1);
-    }else{
-        return binarysearch(nums,target,mid+1,high);
-    }
-}
+// int binarysearch(vector<int>&nums, int target, int low, int high){
+//     if (low>high){
+//         return -1;
+//     }
+//     int mid = (low+high)/2;
+//     if (nums[mid]==target) return mid;
+//     else if (nums[mid]>target){
+//         return binarysearch(nums,target,low,mid-1);
+//     }else{
+//         return binarysearch(nums,target,mid+1,high);
+//     }
+// }
 
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         return binarysearch(nums,target,0,nums.size()-1);
+//     }
+// };
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return binarysearch(nums,target,0,nums.size()-1);
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (nums[mid]==target) return mid;
+            else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return -1;
     }
 };
