befbf9da5d46c25114c1d7acb7ea4c5f8e325b93
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-01-14 00:49:24 +0530

    Runtime: 0 ms (100.00%), Memory: 52.4 MB (27.24%) - Gitcode

--- 55-jump-game/55. Jump Game.cpp
+++ 55-jump-game/55. Jump Game.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int index_max_reached=0; int cur_index=0;
-        if (nums[0]==0 && nums.size()>1) return false;
+        int index_max_reached=0;
+        // if (nums[0]==0 && nums.size()>1) return false;
         for (int i=0; i<nums.size()-1; i++){
             // cout<<"Now the max possible reach="<<index_max_reached<<" and cur_index="<<cur_index<<endl;
             if (nums[i]==0 && index_max_reached<=i ) break;
-            index_max_reached = max(index_max_reached, cur_index+nums[i]);
-            cur_index++;
+            index_max_reached = max(index_max_reached, i+nums[i]);
         }
         // cout<<"Now the max possible reach="<<index_max_reached<<" and cur_index="<<cur_index<<endl;
         if (index_max_reached >= nums.size()-1) return true;
