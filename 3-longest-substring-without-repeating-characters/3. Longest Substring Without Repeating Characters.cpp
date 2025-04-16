5af2990659766abb81befeb53872c53e5f11e373
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-04-16 16:43:07 +0530

    Runtime: 15 ms (41.55%), Memory: 12.4 MB (42.67%) - Gitcode

--- 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
@@ -1,20 +1,28 @@
+#include <set>
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ptr1 = 0;
-        int ptr2 = 0;
-        int max_len = 0;
-        set<char> st;
-        while (ptr2< s.size()){
-            if (st.find(s[ptr2]) == st.end()){
-                st.insert(s[ptr2]);
-                max_len = max(max_len, ptr2 - ptr1 +1);
-                ptr2++;
-            }else {
-                st.erase(s[ptr1]);
+        int ptr1=0; int ptr2=0;
+        unordered_set<char> se;
+        int len = 0;
+        while (ptr2<s.size() && ptr2>=ptr1){
+            if (se.find(s[ptr2]) != se.end()){
+                // se.clear();
+                while (s[ptr1] != s[ptr2]){
+                    se.erase(s[ptr1]);
+                    ptr1++;
+                }
+                se.insert(s[ptr2]);
                 ptr1++;
+            }else{
+                se.insert(s[ptr2]);
+                len = max(len, (int)se.size());
             }
+            // cout<<"The ptr2="<<ptr2<<", and the ptr1="<<ptr1<<"The elements of se=";
+            // for (auto &i:se) cout<<i<<", ";
+            // cout<<endl;
+            ptr2++;
         }
-        return max_len;
+        return len;
     }
 };
