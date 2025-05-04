67e7fddf0a97bc580b0cf6805b5cf46a1e5a8acd
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-05-04 17:56:03 +0530

    Runtime: 6 ms (43.75%), Memory: 26.2 MB (48.50%) - Gitcode

---
+++ 1128-number-of-equivalent-domino-pairs/1128. Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,37 @@
+/////// O(n^2) Solution->
+// class Solution {
+// public:
+//     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+//         int count = 0;
+//         for (int i=0; i<dominoes.size(); i++){
+//             sort(dominoes[i].begin(), dominoes[i].end());
+//             vector<int>temp = dominoes[i];
+//             // cout<<"The temp = ";
+//             // for(auto &k:temp) cout<<k<<" ";
+//             // cout<<endl; 
+//             for (int j=i+1; j<dominoes.size(); j++){
+//                 sort(dominoes[j].begin(),dominoes[j].end());
+//                 if (temp == dominoes[j]){
+//                     count++;
+//                 }  
+//             }
+//         }
+//         return count;
+//     }
+// };
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int count = 0;
+        map<vector<int>,int>mp;
+        for (int i=0; i<dominoes.size(); i++){
+            sort(dominoes[i].begin(),dominoes[i].end());
+            mp[dominoes[i]]++;    
+        }
+        for (auto &i:mp){
+            int temp_count = i.second;
+            count+= (temp_count*(temp_count-1))/2;
+        }
+        return count;
+    }
+};
