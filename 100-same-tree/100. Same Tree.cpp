4aa34008bf63259ea62ea86a329e557e05343867
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-05-07 14:23:16 +0530

    Runtime: 0 ms (100.00%), Memory: 13.1 MB (15.26%) - Gitcode

--- 100-same-tree/100. Same Tree.cpp
+++ 100-same-tree/100. Same Tree.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     void pre(TreeNode* root,vector<int>&arr ){
         if (!root) { 
-            arr.push_back(INT_MIN);
+            arr.push_back(INT_MIN); // This so that we can differentiate between the individual points;
             return ;
         }
         arr.push_back(root->val);
@@ -24,8 +24,8 @@ public:
         // Just check the pre-order traversal of both the roots.
         vector<int>ps; pre(p, ps);
         vector<int>qs; pre(q, qs);
-        cout<<"NOw printing p"<<endl; for (auto& i:ps) cout<<i<<","; cout<<endl;
-        cout<<"NOw printing q"<<endl; for (auto& i:qs) cout<<i<<","; cout<<endl;
+        // cout<<"NOw printing p"<<endl; for (auto& i:ps) cout<<i<<","; cout<<endl;
+        // cout<<"NOw printing q"<<endl; for (auto& i:qs) cout<<i<<","; cout<<endl;
         return ps==qs;
     }
 };
