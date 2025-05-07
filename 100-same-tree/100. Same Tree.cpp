e3245dd553590c8f6641efd18b418e8554e7c4f9
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-05-07 14:22:44 +0530

    Runtime: 3 ms (0.83%), Memory: 12.9 MB (15.26%) - Gitcode

---
+++ 100-same-tree/100. Same Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void pre(TreeNode* root,vector<int>&arr ){
+        if (!root) { 
+            arr.push_back(INT_MIN);
+            return ;
+        }
+        arr.push_back(root->val);
+        pre(root->left, arr);
+        pre(root->right, arr);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // Just check the pre-order traversal of both the roots.
+        vector<int>ps; pre(p, ps);
+        vector<int>qs; pre(q, qs);
+        cout<<"NOw printing p"<<endl; for (auto& i:ps) cout<<i<<","; cout<<endl;
+        cout<<"NOw printing q"<<endl; for (auto& i:qs) cout<<i<<","; cout<<endl;
+        return ps==qs;
+    }
+};
