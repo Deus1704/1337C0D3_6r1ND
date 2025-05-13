66373fcbbe79eb9d3807c7c8387ec57a1a67889a
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-05-13 13:43:21 +0530

    Runtime: 19 ms (5.11%), Memory: 19.1 MB (99.75%) - Gitcode

--- 547-number-of-provinces/547. Number of Provinces.cpp
+++ 547-number-of-provinces/547. Number of Provinces.cpp
@@ -1,34 +1,32 @@
 class Solution {
-    void bfs(vector<vector<int>>&adj, vector<int>&visited, int root, int &count_province){
-        queue<int>q;q.push(root);
-        if (visited[root]==1) return;
-        visited[root]=1;
-        while (!q.empty()){
-            int top_node = q.front(); q.pop();
-            for (auto &i:adj[top_node]){
-                if (visited[i]==0) {
-                    visited[i]=1;
-                    q.push(i);
-                }
+private:
+    void dfs(vector<int> adjList[], int node, vector<int>& visited){
+        visited[node] = 1;
+        for (auto it : adjList[node]){
+            if (!visited[it]){
+                dfs(adjList, it, visited);
             }
         }
-        count_province++;
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>>adj(isConnected.size()+1);
-        for (int i=1; i<=isConnected.size(); i++){
-            for (int j=0; j<isConnected[i-1].size(); j++){
-                if (isConnected[i-1][j] && i!=j+1){
-                    adj[i].push_back(j+1);
+        int n = isConnected.size();
+        vector<int> adjList[n];
+        for (int i =0; i< n;i++){
+            for (int j=0; j<n;j++){
+                if (isConnected[i][j] && i!=j){
+                    adjList[i].push_back(j);
                 }
             }
         }
-        // Now using BFS on all the nodes to get the total number of provinces
-        vector<int>visited(isConnected.size()+1, 0); int count_province=0;
-        for (int i=1; i<=isConnected.size(); i++){
-            bfs(adj,visited, i, count_province);
+        vector<int> visited(n,0);
+        int count=0;
+        for (int i =0; i< n; i++){
+            if (!visited[i]){
+                dfs(adjList, i, visited);
+                count++;
+            }
         }
-        return count_province;
+        return count;
     }
 };
