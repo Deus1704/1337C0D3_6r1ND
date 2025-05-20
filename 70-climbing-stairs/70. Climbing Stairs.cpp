23b3ca52d51a723f50c2fb70d2a3432ca6c45f55
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-05-20 20:29:15 +0530

    Runtime: 3 ms (0.51%), Memory: 7.5 MB (100.00%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.cpp
+++ 70-climbing-stairs/70. Climbing Stairs.cpp
@@ -1,19 +1,22 @@
+int rec(int level, int n, vector<int>& memo){
+    if (level>n) return 0;
+    if (level == n) return 1;
+    if (memo[level]!=-1) return memo[level];
+    int ans = 0;
+    for (auto i=1; i<3; i++){
+        if (level+i <= n){
+            int way = rec(level+i, n, memo);
+            ans+= way;
+        }
+    }
+    memo[level] = ans;
+    return ans;
+}
+
 class Solution {
 public:
-    int rec(int i, int n, vector<int>&arr){
-        //base case
-        if (i==n) return 1; // 1 cause if I am on the n'th stair then it makes a possible case
-        else if (i>n) return 0;
-        // Recurse and check all ways;
-        if (arr[i]!=0) return arr[i];
-        int ans= 0;
-        for (int j=1; j<3; j++){
-            if (i+j<=n) ans+=rec(i+j,n, arr);
-        }
-        return arr[i]=ans;
-    }
     int climbStairs(int n) {
-        vector<int>arr(45,0);
-        return rec(0,n, arr);    
+        vector<int> memo(n+1,-1);
+        return rec(0,n, memo);
     }
 };
