d0888f7a59b91d55ae007965d1bbccacf64a9241
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-06-12 23:32:23 +0530

    Runtime: 46 ms (61.46%), Memory: 18.7 MB (86.65%) - Gitcode

--- 1048-longest-string-chain/1048. Longest String Chain.cpp
+++ 1048-longest-string-chain/1048. Longest String Chain.cpp
@@ -1,31 +1,34 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    bool check_possible(string &s1, string &s2){
-        if (s1.size()!=s2.size()+1) return false;
-        int first=0,second=0;
+    bool difbychar(string &s1,string &s2 ){
+        if (s1.size() != s2.size()+1) return false;
+        int first=0, second =0;
         while (first!=s1.size()){
-            if (second<s2.size() && s1[first]==s2[second]){
+            if (second <= s2.size() && s1[first]==s2[second]){
                 first++;second++;
             }else{
                 first++;
             }
         }
-        if (first==s1.size() && second==s2.size()) return true;
+        if (first==s1.size() && second == s2.size()) return true;
         else return false;
     }
+    // since we just need to sort by the length and not its total value.
+    static bool comp(string &s1, string &s2){// This needs to be static here since this comparator, has an extra Solution this pointer that is not needed by the standard sort() func. 
+        return s1.size()<s2.size();
+    }
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
-        sort(words.begin(),words.end(), [](string &s1, string &s2){ return s1.size()<s2.size();});
-        vector<int>dp(n,1);int max_len = 0;
+        int n = words.size(), ans = 0;
+        sort(words.begin(), words.end(), comp);
+        vector<int>dp(n,1);
         for (int i=0; i<n; i++){
-            for (int prev = 0; prev<i; prev++){
-                if (check_possible(words[i],words[prev]) && 1+dp[prev]>dp[i]){
-                    dp[i]=1+dp[prev];
+            for (int j=0; j<i; j++){
+                if (difbychar(words[i],words[j]) && dp[j]+1>dp[i]){
+                    dp[i] = 1+dp[j];
                 }
             }
-            max_len = max(max_len, dp[i]);
+            if (dp[i]>ans) ans = dp[i];
         }
-        return max_len;
+        return ans;
     }
 };
