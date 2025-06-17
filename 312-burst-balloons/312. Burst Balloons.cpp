58c511816619ab79cecce69b1b0c82a08309cfb0
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-06-17 12:34:54 +0530

    Runtime: 202 ms (37.80%), Memory: 14 MB (6.86%) - Gitcode

--- 312-burst-balloons/312. Burst Balloons.cpp
+++ 312-burst-balloons/312. Burst Balloons.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int rec(int i, int j,vector<int>& a,vector<vector<int>>& dp ){
-        // base case
+    int rec(int i, int j, vector<int>&a, vector<vector<int>>&dp ){
+        // base case;
         if (i>j) return 0;
-        // cache check
         if (dp[i][j]!=-1) return dp[i][j];
-        // try all cases
-        int maxy = INT_MIN;
+        int max_cost = INT_MIN;
         for (int index=i; index<=j; index++){
-            int coin = a[i-1]*a[index]*a[j+1] + rec(i,index-1,a,dp)+rec(index+1,j,a,dp);
-            maxy=max(maxy, coin);
+            int cost = a[i-1]*a[index]*a[j+1] + rec(i,index-1, a,dp) + rec(index+1,j, a,dp);
+            max_cost = max(max_cost, cost);
         }
-        return dp[i][j]=maxy;
+        return dp[i][j]=max_cost;
     }
-    int maxCoins(vector<int>& a) {
-        a.push_back(1);a.insert(a.begin(),1);
-        int n=a.size();
+    int maxCoins(vector<int>& nums) {
+        nums.push_back(1);nums.insert(nums.begin(),1);
+        int n=nums.size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return rec(1,n-2,a,dp);
+        return rec(1,n-2, nums,dp);
     }
 };
