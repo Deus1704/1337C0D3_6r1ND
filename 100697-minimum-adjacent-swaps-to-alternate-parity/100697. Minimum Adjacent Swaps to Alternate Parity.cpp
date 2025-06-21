e7a9a2ebc0659363a4b7d647a13f76001cab0e02
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-06-21 20:40:12 +0530

    Runtime: 12 ms (70.00%), Memory: 201.1 MB (20.00%) - Gitcode

--- 100697-minimum-adjacent-swaps-to-alternate-parity/100697. Minimum Adjacent Swaps to Alternate Parity.cpp
+++ 100697-minimum-adjacent-swaps-to-alternate-parity/100697. Minimum Adjacent Swaps to Alternate Parity.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    int cost_cal(vector<int> ar, int start){
+        int cost=0;
+        for (int k = 0; k < ar.size(); k++) {
+            cost += abs(ar[k] -  (start+2*k));
+        }
+        return cost;
+    }
     int minSwaps(vector<int>& nums) {
         int c1 = 0, c2 = 0, n = nums.size();
         vector<int> oddPos, evenPos;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] % 2) {
-                ++c1;
+                c1++;
                 oddPos.push_back(i);
             } else {
-                ++c2;
+                c2++;
                 evenPos.push_back(i);
             }
         }
         if (abs(c1 - c2) > 1) return -1;
-        auto cost = [&](const vector<int>& pos, int start) {
-            int cnt = 0;
-            for (int k = 0; k < pos.size(); ++k) {
-                cnt += abs(pos[k] - (start + 2*k));
-            }
-            return cnt;
-        };
-        if (c1 > c2) return (int)cost(oddPos, 0);
-        if (c2 > c1) return (int)cost(evenPos, 0);
-        return (int)min(cost(oddPos, 0), cost(evenPos, 0));
+        int cost = 0;
+        if (c1 > c2) {
+            cost= cost_cal(oddPos, 0);
+        }else if (c2>c1){
+            cost = cost_cal(evenPos, 0);
+        }else{
+            cost = min(cost_cal(oddPos, 0), cost_cal(evenPos, 0));
+        }
+        return cost;
     }
 };
