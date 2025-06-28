daae41b39bf484397a504bbdc786aee6c57375ef
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-06-28 15:00:13 +0530

    Runtime: 12 ms (34.88%), Memory: 41.7 MB (13.91%) - Gitcode

---
+++ 1020-number-of-enclaves/1020. Number of Enclaves.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(int i,int j, vector<vector<int>>& g, vector<vector<int>>& vis, vector<vector<int>>& pos, vector<pair<int,int>>&d){
+        vis[i][j]=1;pos[i][j]=1;
+        for (auto [dx,dy]:d){
+            int x = i+dx, y = j+dy;
+            if ( (x>=0 && x<g.size()) && (y>=0 && y<g[0].size()) && !vis[x][y] && g[x][y]==1){
+                dfs(x,y,g,vis,pos,d);
+            }
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(), count=0;
+        vector<pair<int,int>>d = {{-1,0},{0,1},{1,0},{0,-1}};
+        vector<vector<int>>vis(n,vector<int>(m,0)), possible(n,vector<int>(m,0));
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if ( (i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1){
+                    dfs(i,j,grid, vis, possible, d);
+                }
+            }
+        }
+        // for (auto i:possible){
+        //     cout<<"now printing row="; for (auto j:i) cout<<j<<","; cout<<endl;
+        // }
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if (grid[i][j]==1 && !possible[i][j]) count++;
+            }
+        }
+        return count;
+    }
+};
