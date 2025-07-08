619b2ee3b71ba3695ea178be0ef5e0b9892f15b5
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-07-08 19:40:25 +0530

    Runtime: 0 ms (100.00%), Memory: 7.8 MB (53.23%) - Gitcode

--- 231-power-of-two/231. Power of Two.cpp
+++ 231-power-of-two/231. Power of Two.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int count_set_bits = 0; 
-        if (n<0) return false;
-        while (n){
-            if (n&1) count_set_bits++;
-            n>>=1;
-        }
-        if (count_set_bits==1) return true;
-        else return false;
+        // int count_set_bits = 0; 
+        // if (n<0) return false;
+        // while (n){
+        //     if (n&1) count_set_bits++;
+        //     n>>=1;
+        // }
+        // if (count_set_bits==1) return true;
+        // else return false;
+        return ( n>0 && (n&(n-1)) ==0);
     }
 };
