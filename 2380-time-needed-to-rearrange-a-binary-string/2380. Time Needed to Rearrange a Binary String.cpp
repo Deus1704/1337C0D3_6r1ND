b0c72444745062905812822b269dd0d29856113b
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-07-22 17:22:33 +0530

    Runtime: 55 ms (29.80%), Memory: 9.4 MB (30.54%) - Gitcode

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380. Time Needed to Rearrange a Binary String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int secondsToRemoveOccurrences(string s) {
+        int timer=0, n=s.size(); bool cont = true;
+        string reversed_s = s, sorted_s=s; 
+        reverse(reversed_s.begin(), reversed_s.end());
+        sort(sorted_s.begin(), sorted_s.end());
+        if (reversed_s == sorted_s) return 0;
+        while (cont){
+            for (int i=0; i<n-1; i++){
+                if (s[i]=='0' && s[i+1]=='1'){
+                    s[i]= '1'; s[i+1] = '0'; i++;
+                }
+            }
+            for (int i=0; i<n-1; i++) {
+                if (s[i]=='0' && s[i+1]=='1') { cont = true; break;}
+                else cont = false;
+            }
+            timer++;
+        }
+        return timer;
+    }
+};
