f11617518d1ce693007f2d72f395533d2499e6f5
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-07-22 17:33:48 +0530

    Runtime: 0 ms (100.00%), Memory: 9.3 MB (60.10%) - Gitcode

--- 2380-time-needed-to-rearrange-a-binary-string/2380. Time Needed to Rearrange a Binary String.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380. Time Needed to Rearrange a Binary String.cpp
@@ -1,22 +1,36 @@
+// class Solution {
+// public:
+//     int secondsToRemoveOccurrences(string s) {
+//         int timer=0, n=s.size(); bool cont = true;
+//         string reversed_s = s, sorted_s=s; 
+//         reverse(reversed_s.begin(), reversed_s.end());
+//         sort(sorted_s.begin(), sorted_s.end());
+//         if (reversed_s == sorted_s) return 0;
+//         while (cont){
+//             for (int i=0; i<n-1; i++){
+//                 if (s[i]=='0' && s[i+1]=='1'){
+//                     s[i]= '1'; s[i+1] = '0'; i++;
+//                 }
+//             }
+//             for (int i=0; i<n-1; i++) {
+//                 if (s[i]=='0' && s[i+1]=='1') { cont = true; break;}
+//                 else cont = false;
+//             }
+//             timer++;
+//         }
+//         return timer;
+//     }
+// };
+
 class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
-        int timer=0, n=s.size(); bool cont = true;
-        string reversed_s = s, sorted_s=s; 
-        reverse(reversed_s.begin(), reversed_s.end());
-        sort(sorted_s.begin(), sorted_s.end());
-        if (reversed_s == sorted_s) return 0;
-        while (cont){
-            for (int i=0; i<n-1; i++){
-                if (s[i]=='0' && s[i+1]=='1'){
-                    s[i]= '1'; s[i+1] = '0'; i++;
-                }
+        int timer=0, n=s.size(), zero_count = 0;
+        for (int i=0; i<n; i++){
+            if (s[i]=='0') zero_count++;
+            else if (zero_count>0){
+                timer = max(timer+1, zero_count);
             }
-            for (int i=0; i<n-1; i++) {
-                if (s[i]=='0' && s[i+1]=='1') { cont = true; break;}
-                else cont = false;
-            }
-            timer++;
         }
         return timer;
     }
