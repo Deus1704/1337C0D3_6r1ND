b66150d4e7a9ee0d35d9e2e2c1576fd408852d36
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-07-28 19:41:49 +0530

    Runtime: 0 ms (100.00%), Memory: 65.1 MB (69.88%) - Gitcode

--- 287-find-the-duplicate-number/287. Find the Duplicate Number.cpp
+++ 287-find-the-duplicate-number/287. Find the Duplicate Number.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    // int findDuplicate(vector<int>& nums) {
+    //     int ptr=0, n=nums.size();
+    //     for (int i=0; i<n; i++){
+    //         if (nums[abs(nums[i])] <0 ){
+    //             // found the repeated number
+    //             ptr = i;
+    //             break;
+    //             // return abs(nums[abs(nums[i])]);
+    //         }else{
+    //             nums[abs(nums[i])] *= -1;
+    //         }
+    //     }
+    //     for (auto &i:nums) if (i<0) i*=-1;
+    //     return nums[ptr];
+    // }
     int findDuplicate(vector<int>& nums) {
-        int ptr=0, n=nums.size();
-        for (int i=0; i<n; i++){
-            if (nums[abs(nums[i])] <0 ){
-                // found the repeated number
-                ptr = i;
-                break;
-                // return abs(nums[abs(nums[i])]);
-            }else{
-                nums[abs(nums[i])] *= -1;
-            }
+        int fast = nums[0], slow = nums[0]; bool first = true;
+        while (slow != fast || first){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (first) first = false;
         }
-        for (auto &i:nums) if (i<0) i*=-1;
-        return nums[ptr];
+        // we are gaurantee to find a cycle since PHP suggests that since there are n+1 holes and n types 
+        // of items so one of the holes would have an item same as 1-n 
+        
+        // Now reset the slow to start; The fast is still inside the loop
+        slow = nums[0];
+        // now keep moving, till the fast and slow meet
+        while (fast != slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
