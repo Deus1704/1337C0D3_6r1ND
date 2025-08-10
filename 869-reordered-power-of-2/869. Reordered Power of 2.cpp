9e03178e47c5bed72d7c7eb1946eb80868d1cd38
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-08-10 14:32:26 +0530

    Runtime: 3 ms (23.60%), Memory: 9.6 MB (21.53%) - Gitcode

---
+++ 869-reordered-power-of-2/869. Reordered Power of 2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        set<string>temp;
+        for (int i=0; i<34; i++){
+            string power_of_two = to_string(1LL<<i); sort(power_of_two.begin(), power_of_two.end());
+            temp.insert(power_of_two);
+        }
+        // for (auto &i:temp) cout<<i<<","; cout<<endl;
+        string ip = to_string(n); sort(ip.begin(), ip.end());
+        if (temp.count(ip)) return true;
+        return false;
+    }
+};
