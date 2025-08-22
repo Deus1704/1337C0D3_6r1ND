23c44979b0a7a5f19c8dae1acfe99a1cb6dfef69
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2025-08-22 23:02:49 +0530

    Runtime: 0 ms (100.00%), Memory: 31.9 MB (67.80%) - Gitcode

--- 173-binary-search-tree-iterator/173. Binary Search Tree Iterator.cpp
+++ 173-binary-search-tree-iterator/173. Binary Search Tree Iterator.cpp
@@ -9,32 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+    // Simple Pre Computation of the Inorder Traversal and then just return whats needed.
+    // This has TC-> ~O(1) But SC-> O(N) since storing in a vector.
+// class BSTIterator {
+// public:
+//     void rec(TreeNode* root, vector<int>&inorderd_traversal){
+//         if (!root) return;
+//         if (root->left) rec(root->left, inorderd_traversal);
+//         inorderd_traversal.push_back(root->val);
+//         if (root->right) rec(root->right, inorderd_traversal);
+//     }
+//     int ptr;
+//     vector<int>inorderd_traversal;
+//     BSTIterator(TreeNode* root) {
+//         auto temp = root;
+//         rec(root, inorderd_traversal);
+//         ptr = 0;
+//     }
+    
+//     int next() {
+//         if (ptr < inorderd_traversal.size()){
+//             int to_return = inorderd_traversal[ptr];
+//             ptr++;
+//             return to_return;
+//         }else return -1;
+//     }
+    
+//     bool hasNext() {
+//         return (ptr<inorderd_traversal.size());
+//     }
+// };
+
 class BSTIterator {
 public:
-    void rec(TreeNode* root, vector<int>&inorderd_traversal){
-        if (!root) return;
-        if (root->left) rec(root->left, inorderd_traversal);
-        inorderd_traversal.push_back(root->val);
-        if (root->right) rec(root->right, inorderd_traversal);
-    }
-    int ptr;
-    vector<int>inorderd_traversal;
+    stack<TreeNode*>inorder_track;
     BSTIterator(TreeNode* root) {
         auto temp = root;
-        rec(root, inorderd_traversal);
-        ptr = 0;
+        while (temp){
+            inorder_track.push(temp); temp = temp->left;
+        }
+        
     }
     
     int next() {
-        if (ptr < inorderd_traversal.size()){
-            int to_return = inorderd_traversal[ptr];
-            ptr++;
-            return to_return;
-        }else return -1;
+        auto top = inorder_track.top(); inorder_track.pop();
+        if (top->right){
+            auto another_temp = top->right; inorder_track.push(another_temp);
+            while (another_temp->left){
+                inorder_track.push(another_temp->left); another_temp = another_temp->left;
+            }
+        }
+        return top->val;
     }
     
     bool hasNext() {
-        return (ptr<inorderd_traversal.size());
+        return !inorder_track.empty();
     }
 };
 
